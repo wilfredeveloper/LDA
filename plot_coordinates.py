c44e9e04071028d4faac4b70b7dd85f34a498c8e
wilfredeveloper <precious11120@student.mmarau.ac.ke>
2023-11-21 18:48:06 +0300

    ✨ Plotting the graph
    this python file outputs a graph using matplotlib

---
+++ plot_coordinates.py
@@ -0,0 +1,15 @@
+# plot_coordinates.py
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Read coordinates from file
+coordinates = np.loadtxt('coordinates.txt')
+
+# Plot the graph
+plt.plot(coordinates[:, 0], coordinates[:, 1], marker='o', linestyle='-', color='b')
+plt.title('DDA Line Drawing')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.grid(True)
+plt.show()
